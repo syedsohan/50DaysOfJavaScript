14d784ede0c37319c7f01f00781080d719a0e32e
Syed Sohan <63908584+syedsohan@users.noreply.github.com>
2021-12-20 16:47:50 +0600

    Add Day-23

---
+++ Day-23/findEvenIndex.js
@@ -0,0 +1,13 @@
+function findEvenIndex(arr) {
+  //Code goes here!
+  for (let i = 0; i < arr.length; i++) {
+    let leftSum = 0;
+    let rightSum = 0;
+
+    for (let j = 0; j < i; j++) leftSum += arr[j];
+    for (let j = i + 1; j < arr.length; j++) rightSum += arr[j];
+
+    if (leftSum === rightSum) return i;
+  }
+  return -1;
+}
